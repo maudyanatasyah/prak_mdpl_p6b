d4bbd8df994a25b77757c11c61a608ff5336a145
MaudyAnatasyah <tasyahpratama2525@gmail.com>
2021-11-03 14:22:54 +0700

    Menambah file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MDPL_PRAK_6</title>
+</head>   
+<body>
+        <h3>Materi Git Remote</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam dicta neque soluta fugit, maiores molestias perferendis quia eaque, expedita, quae sapiente quidem illum in placeat odit rem explicabo provident odio?
+</body>
+</html>
